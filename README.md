b690fefb691122d6b3ed8b040ba3d695012955f7
Evan Dumas <eedumas@gmail.com>
2022-08-07 19:35:40 -0700

    Create initial div

---
+++ README.md
@@ -0,0 +1,3 @@
+Just a little app that will be challenging but fun to make. The goals are at (https://www.theodinproject.com/lessons/foundations-etch-a-sketch).
+
+My side goals include organizing the grid not as squares, but as nested circles, with a little fun math that makes sure they never touch.
---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+// for down the road, spheres can be organized on a grid of triangles with the following equation.
+/*
+    r = radius
+    s = space between circles (10% of r perhaps)
+    w = width between center of circles on a row
+    h = height between center of circles 
+
+    Array them in a grid!
+    w = 2r + s (easy, two half circles and the ideal space between)
+    h = w * sqRt 3 / 2 (so says an online equilateral triangle formula)
+
+*/
+
