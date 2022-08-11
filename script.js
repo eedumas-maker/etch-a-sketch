764c2edde4f29a78f94a790d888d5a4ab6a3ff64
Evan Dumas <eedumas@gmail.com>
2022-08-10 22:53:31 -0700

    Add grid sizing code, but still need to add grid reset logic

--- script.js
+++ script.js
@@ -1,58 +1,72 @@
 const container = document.querySelector('#container');
 const newGridButton = document.querySelector('#newGrid');
 newGridButton.addEventListener('click', () =>  {
-    createGrid(getGridSize(prompt("tell me a number", 16)))
+    createGrid(prompt("tell me a number", 16))
 });
+// great, but it doesn't clear the previous grid, duh
+// it also fails if you enter 0
+// it also doesn't do the math right
 
 createGrid(16);
 
-function getGridSize(text){
+function getGridCount(text){
 
     text = Math.floor(parseInt(text));
 
     newSize = Math.floor(600 / text); //use document size to determine # of squares
+    
+    console.log("text: " + text);
+    console.log("newSize: " + newSize);
 
     return newSize;
 }
 
-function createGrid(gridSize){
 
+function createGrid(gridCount){
+
+
+    let cellSize = getGridCount(gridCount);
+    
     let row = [];
 
     // first make the row
-    for (let i = 1; i <= gridSize; i++){
+    for (let i = 1; i <= gridCount; i++){
         row.push(i);
     }
 
     // now to make an array of each box
-    let grid = new Array(gridSize); // Create initial array
+    let grid = new Array(gridCount); // Create initial array
 
     // loop through array adding arrays for each
-    for (let i = 0; i < gridSize; i++){
-        grid[i] = new Array(gridSize);
+    for (let i = 0; i < gridCount; i++){
+        grid[i] = new Array(gridCount);
     }
 
     // now loop through each grid item and give it coordinates
-    for (let i = 0; i < gridSize; i++) {
-        for (j = 0; j < gridSize; j++){
+    for (let i = 0; i < gridCount; i++) {
+        for (j = 0; j < gridCount; j++){
             grid[i][j] = (i + 1) + ',' + (j + 1);
         }
     }
 
     // let's make that grid!
-    for (let i = 0; i < gridSize; i++){
+    for (let i = 0; i < gridCount; i++){
         row[i] = document.createElement('div');
         row[i].className = 'row';
+        row[i].style.height = cellSize;
+
         container.appendChild(row[i]);
         // so each row has it's own name "row[i]"
 
-        for (let j = 0; j < gridSize; j++) {
+        for (let j = 0; j < gridCount; j++) {
             grid[i][j] = document.createElement('div');
             grid[i][j].className = 'box';
+            grid[i][j].style.height = cellSize;
+            grid[i][j].style.minWidth = cellSize;
+            // change the height and min-width there
+
             row[i].appendChild(grid[i][j]);
 
-            console.log(grid[i][j]); // returns div
-            console.log(typeof grid[i][j]); // returns object
             grid[i][j].addEventListener('mouseover', () => grid[i][j].classList.add('hover'));
         }
     }
